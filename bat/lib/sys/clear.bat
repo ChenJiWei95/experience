58b971ca2f0a46cfc7762a82d876b4e00563dd4a
chen.jiwei <chenjiwey@163.com>
2020-06-25 15:14:40 +0800

    1

--- bat/lib/sys/clear.bat
+++
@@ -1,6 +0,0 @@
-::Date   18/6/2
-::Author 威
-::Desc   本批处理功能：
-::			显示temp.data中的结果
-@echo off
-del temp.data
--- bat/mysql.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-cmd /k "cd /d D:\BtSoft\mysql\MySQL5.5\bin && echo mysql -hlocalhost -uroot -p"
